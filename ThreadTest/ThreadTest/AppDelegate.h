58fad91e9c4c107b8da49f266802579545681f6f
QuanGe <zhang_ru_quan@163.com>
2015-09-27 22:51:18 +0800

    多线程测试
    多线程测试

---
+++ ThreadTest/ThreadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadTest
+//
+//  Created by 张如泉 on 15/9/27.
+//  Copyright © 2015年 QuanGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreadTest/ThreadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadTest
+//
+//  Created by 张如泉 on 15/9/27.
+//  Copyright © 2015年 QuanGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
