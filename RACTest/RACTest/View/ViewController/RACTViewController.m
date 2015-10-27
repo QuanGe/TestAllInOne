4e525087bf36d11f6bb9eca4e3711d5a716017a5
QuanGe <zhang_ru_quan@163.com>
2015-10-27 22:41:13 +0800

    tac 信号例子
    tac 信号例子

--- RACTest/RACTest/View/ViewController/RACTViewController.m
+++ RACTest/RACTest/View/ViewController/RACTViewController.m
@@ -8,7 +8,7 @@
 
 #import "RACTViewController.h"
 #import "UIView+WillChange.h"
-
+#import "ReactiveCocoa.h"
 @interface RACTViewController ()
 
 @end
@@ -19,6 +19,50 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"category返回的view的宽度到底是多少%@",@([self.view width]));
+    
+    //伤害值
+    __block int aNumber = 0;
+    
+    //植物大战僵尸 中的豌豆射手
+    RACSignal *peaKiller = [RACSignal createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+        aNumber++;
+        
+        if(aNumber >2)
+            [subscriber sendError:[NSError errorWithDomain:@"MyDomain" code:0 userInfo:@{NSLocalizedDescriptionKey:@"不好意思，正在冷却"}]];
+        else
+        {
+            //发射豌豆子弹
+            [subscriber sendNext:@(aNumber)];
+            [subscriber sendCompleted];
+        }
+            
+        return [RACDisposable disposableWithBlock:^{
+            aNumber = 0;
+            NSLog(@"进行清理工作");
+        }];;
+    }];
+    
+    //对豌豆射手 添加一个靶子 比如僵尸 （这里用block 生成一个RACSubscriber对象，block内的代码 就是接受到子弹后处理）
+    [peaKiller subscribeNext:^(id x) {
+        NSLog(@"来了一只小僵尸，对僵尸1减去生命值 %@，直接干死", x);
+    }];
+    
+    //map 相当于对子弹进行加工 比如加上寒冰属性 使伤害值加100
+    RACDisposable * dispose = [[peaKiller map:^id(id value) {
+        return @([value integerValue] +100);
+    }] subscribeNext:^(id x) {
+        NSLog(@"又来一只boss，对僵尸2减去生命值 %@，直接干死", x);
+    }];
+    
+    [peaKiller subscribeNext:^(id x) {
+        NSLog(@"玩了又来了一只，这次还有子弹么 %@", x);
+    } error:^(NSError *error) {
+        NSLog(@"%@",NSLocalizedString([error userInfo][NSLocalizedDescriptionKey], nil));
+    }];
+    
+    //游戏结束 进行清理工作
+    [dispose dispose];
+    
 }
 
 - (void)didReceiveMemoryWarning {
