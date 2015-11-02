7eaa87d0b53281e5741a6271daf9cac038b866d2
QuanGe <zhang_ru_quan@163.com>
2015-11-02 23:53:15 +0800

    model 细化
    model 细化

--- programmermag/programmermag/Model/PMArticleImageModel.h
+++ programmermag/programmermag/Model/PMArticleImageModel.h
@@ -7,7 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-
+typedef NS_ENUM(NSInteger,PMArticleImageType)
+{
+    PMArticleImageTypeContentNoImage,
+    PMArticleImageTypeContentImage,
+    PMArticleImageTypeContentCodeLink,
+    PMArticleImageTypeContentStaticImage,
+    PMArticleImageTypeContentHomeImage,
+    PMArticleImageTypeContentVideo
+} ImageType;
 @interface PMArticleImageModel : NSObject
-
+@property (nonatomic,readwrite,assign) PMArticleImageType type;
+@property (nonatomic,readwrite,strong) NSString* imageFileName;
+@property (nonatomic,readwrite,strong) NSString* cickImageFileName;
+@property (nonatomic,readwrite,strong) NSString* imageCaption;
+@property (nonatomic,readwrite,assign) float height;
+@property (nonatomic,readwrite,assign) int colspan;
+@property (nonatomic,readwrite,assign) int location;
 @end
--- programmermag/programmermag/Model/PMArticleModel.h
+++ programmermag/programmermag/Model/PMArticleModel.h
@@ -7,7 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
-
+@class PMArticleImageModel;
 @interface PMArticleModel : NSObject
+@property (nonatomic,strong) NSString* titile;
+@property (nonatomic,strong) NSString* subTitile;
+@property (nonatomic,strong) NSString* editor;
+@property (nonatomic,strong) NSString* articleDescription;
+@property (nonatomic,strong) NSString* type;
+@property (nonatomic,strong) NSString* mcategory;
+@property (nonatomic,strong) NSString* pubDate;
+
+@property (nonatomic,strong) NSMutableAttributedString* content;
+@property (nonatomic,strong) NSMutableAttributedString* headerStr;
+@property (nonatomic,strong) NSMutableArray* images;
+@property (nonatomic,strong) PMArticleImageModel* staticImage;
 
 @end
