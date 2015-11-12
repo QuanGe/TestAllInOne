5b40e8c42ed203113e7b85db57dd00f62d7e7bcb
QuanGe <zhang_ru_quan@163.com>
2015-11-12 09:25:52 +0800

    tableView 的例子
    tableView 的例子

---
+++ TableViewTest/TableViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewTest
+//
+//  Created by 张如泉 on 15/11/12.
+//  Copyright © 2015年 quange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewTest/TableViewTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewTest
+//
+//  Created by 张如泉 on 15/11/12.
+//  Copyright © 2015年 quange. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
