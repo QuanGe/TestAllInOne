7fcee9e7e6a5eddef645c43cb6ae57d684b666ae
QuanGe <zhang_ru_quan@163.com>
2015-11-13 18:11:25 +0800

    圆角 性能测试
    圆角 性能测试

--- TableViewTest/TableViewTest/ViewController.m
+++ TableViewTest/TableViewTest/ViewController.m
@@ -7,7 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "UIButton+AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+#import "QGOCCategory.h"
+#import "ReactiveCocoa.h"
 @interface ViewController ()
 
 @end
@@ -24,4 +27,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"cornerRadiusCell"];
+    
+    
+    for (int i = 12; i< 18; ++i) {
+        UIView * btn = [cell.contentView viewWithTag:i];
+        
+        /*
+        CAShapeLayer *layer = [CAShapeLayer layer];
+        UIBezierPath *aPath = [UIBezierPath bezierPathWithOvalInRect:btn.bounds];
+        layer.path = aPath.CGPath;
+        btn.layer.mask = layer;*/
+        
+        //btn.layer.cornerRadius =20;
+        //btn.layer.masksToBounds = YES;
+        //btn.layer.shouldRasterize = YES;
+        //btn.layer.rasterizationScale = [UIScreen mainScreen].scale;
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://avatar.csdn.net/C/C/C/1_ghostbear.jpg"]];
+        if(i == 12)
+        {
+            @weakify(btn);
+            [(UIButton *)btn setImageForState:UIControlStateNormal withURLRequest:request placeholderImage:[[UIImage imageNamed:@"festival_d11_detail_tmall"] qgocc_clipImageWithCornerRadius:20] success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull image) {
+                @strongify(btn);
+                [(UIButton*)btn setImage:[image qgocc_clipImageWithCornerRadius:20] forState:UIControlStateNormal];
+            } failure:^(NSError * _Nonnull error) {
+                
+            }];
+           
+        }
+        else
+        {
+            @weakify(btn);
+            [(UIImageView *)btn setImageWithURLRequest:request placeholderImage:[[UIImage imageNamed:@"festival_d11_detail_tmall"] qgocc_clipImageWithCornerRadius:20] success:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, UIImage * _Nonnull image) {
+                @strongify(btn);
+                [(UIImageView *)btn setImage:[image qgocc_clipImageWithCornerRadius:20]];
+            } failure:^(NSURLRequest * _Nonnull request, NSHTTPURLResponse * _Nullable response, NSError * _Nonnull error) {
+                
+            } ];
+          
+        }
+    }
+   
+    return cell;
+    
+}
 @end
