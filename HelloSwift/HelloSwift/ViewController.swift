3f5c303003acd1c66d0cc6a725a5dd3730458557
QuanGe <zhang_ru_quan@163.com>
2016-02-26 10:47:33 +0800

    swift 第一个项目创建 真机测试 可以在iOS7.0 运行
    swift 第一个项目创建 真机测试 可以在iOS7.0 运行

---
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by 张如泉 on 16/2/26.
+//  Copyright © 2016年 quange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
