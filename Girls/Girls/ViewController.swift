399b7b0c5e442f36c89c1586985e46e1351489e1
QuanGe <zhang_ru_quan@163.com>
2016-03-07 17:16:52 +0800

    swift 项目创建
    swift 项目创建

---
+++ Girls/Girls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Girls
+//
+//  Created by 张如泉 on 16/3/7.
+//  Copyright © 2016年 quange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
