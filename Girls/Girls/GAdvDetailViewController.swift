59b014ccd978d3aa06efabb9ec76b94fda6d04cc
QuanGe <zhang_ru_quan@163.com>
2016-03-29 09:09:16 +0800

    广告页详情 显示图片
    广告页详情 显示图片

--- Girls/Girls/GAdvDetailViewController.swift
+++ Girls/Girls/GAdvDetailViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 import MBProgressHUD
 import ReactiveCocoa
 import Kanna
-class GAdvDetailViewController: UIViewController {
+import Kingfisher
+class GAdvDetailViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var advsCollectionView: UICollectionView!
+    @IBOutlet weak var advsPageControl: UIPageControl!
     var photos = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true;
+        self.backBtn.layer.cornerRadius = 20
+        self.backBtn.layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).CGColor
+        self.backBtn.titleEdgeInsets = UIEdgeInsets(top: -5, left: 0, bottom: 0, right: 0)
+        self.backBtn.rac_command = RACCommand(signalBlock: { (sender) -> RACSignal! in
+            self.navigationController?.popViewControllerAnimated(true)
+            
+            return RACSignal.empty()
+        })
         MobClick.event("splashAdvDetail")
+        self.advsPageControl.numberOfPages = 1;
+        self.advsPageControl.currentPage = 0;
         self.showLoadingHUD()
         let imageClick = NSUserDefaults.standardUserDefaults().objectForKey("appSplashClick") as? String
         
@@ -45,6 +60,12 @@ class GAdvDetailViewController: UIViewController {
                     }
                     
                 }
+                dispatch_async(dispatch_get_main_queue()) {
+                   self.advsCollectionView.reloadData()
+                   self.advsPageControl.numberOfPages = self.photos.count
+                   self.advsPageControl.currentPage = 0
+                    
+                }
                 
                 NSLog("%@",self.photos )
                 
@@ -72,6 +93,41 @@ class GAdvDetailViewController: UIViewController {
     
     private func hideLoadingHUD() {
         MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+    }
+
+    // MARK: - Collection view data source
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.photos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("advCell", forIndexPath: indexPath)
+        let advImageView = cell.viewWithTag(11) as? UIImageView
+        advsCollectionView.contentMode = .ScaleAspectFill
+        advImageView?.kf_setImageWithURL(NSURL(string:self.photos[indexPath.row])!)
+        self.advsPageControl.currentPage = indexPath.row
+        return cell
+    }
+    //MARK: - UICollectionViewDelegateFlowLayout method
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize{
+        
+        return CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)
+        
+    }
+    
+    //设置四周边距
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 0, 0.0, 0)
+    }
+    
+    //左右间距
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return CGFloat(0)
+    }
+    
+    //    上下间距
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return CGFloat(0)
     }
 
 }
